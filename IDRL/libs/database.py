f84cff4b19e4e60315821536a91f30b372984f7c
Cristhian <ccdelgadop@gmail.com>
2024-04-21 18:45:57 -0500

    add postgress

--- IDRL/libs/database.py
+++ IDRL/libs/database.py
@@ -5,7 +5,16 @@ from sqlmodel import SQLModel, create_engine
 directorio = os.path.abspath(os.getcwd())
 db_directorio = os.path.join(directorio, 'IDRL.db')
 
-engine = create_engine('sqlite:///' + db_directorio, echo=False)
+pg_url = "postgresql://{}:{}@{}:{}/{}".format(
+    "postgres", "postgres", "postgres-db", 5432, "dron_db"
+)
+
+# engine = create_engine('sqlite:///' + db_directorio, echo=False)
+engine = create_engine(
+    pg_url, echo=False
+)
+
+
 
 SQLModel.metadata.create_all(engine)
 
