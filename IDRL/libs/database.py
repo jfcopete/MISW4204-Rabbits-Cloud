47235c96d3a59c83210e729c6a1768f44dd79c30
Cristhian <ccdelgadop@gmail.com>
2024-04-21 23:13:02 -0500

    fix issue

--- IDRL/libs/database.py
+++ IDRL/libs/database.py
@@ -5,14 +5,14 @@ from sqlmodel import SQLModel, create_engine
 directorio = os.path.abspath(os.getcwd())
 db_directorio = os.path.join(directorio, 'IDRL.db')
 
-pg_url = "postgresql://{}:{}@{}:{}/{}".format(
-    "postgres", "postgres", "postgres-db", 5432, "dron_db"
-)
+# pg_url = "postgresql://{}:{}@{}:{}/{}".format(
+#     "postgres", "postgres", "postgres-db", 5432, "dron_db"
+# )
 
-# engine = create_engine('sqlite:///' + db_directorio, echo=False)
-engine = create_engine(
-    pg_url, echo=False
-)
+engine = create_engine('sqlite:///' + db_directorio, echo=False)
+# engine = create_engine(
+#     pg_url, echo=False
+# )
 
 
 
