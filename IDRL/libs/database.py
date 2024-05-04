6bbe6ec210511983d53c236ae24cfcb725f4e4c5
Cristhian <ccdelgadop@gmail.com>
2024-05-04 17:42:52 -0500

    config database

--- IDRL/libs/database.py
+++ IDRL/libs/database.py
@@ -1,18 +1,22 @@
 
 import os
 from sqlmodel import SQLModel, create_engine
+from libs.settings import traer_configuraciones
 
-directorio = os.path.abspath(os.getcwd())
-db_directorio = os.path.join(directorio, 'IDRL.db')
+## connect to sqlLite
+# directorio = os.path.abspath(os.getcwd())
+# db_directorio = os.path.join(directorio, 'IDRL.db')
+# engine = create_engine('sqlite:///' + db_directorio, echo=False)
 
-# pg_url = "postgresql://{}:{}@{}:{}/{}".format(
-#     "postgres", "postgres", "postgres-db", 5432, "dron_db"
-# )
+## connect to postgres
+configuraciones = traer_configuraciones()
+pg_url = "postgresql://{}:{}@{}:{}/{}".format(
+    configuraciones.DB_USER, configuraciones.DB_PASSWORD, configuraciones.DB_HOST, configuraciones.DB_PORT, configuraciones.DB_NAME
+)
+engine = create_engine(
+    pg_url, echo=False
+)
 
-engine = create_engine('sqlite:///' + db_directorio, echo=False)
-# engine = create_engine(
-#     pg_url, echo=False
-# )
-
+# valid for both sqlite and postgres
 SQLModel.metadata.create_all(engine)
 
--- IDRL/libs/settings.py
+++ IDRL/libs/settings.py
@@ -4,6 +4,11 @@ from functools import lru_cache
 class Settings(BaseSettings):
     BUCKET_NAME: str
     CREDENTIAL_FILE: str
+    DB_NAME: str
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_PORT: int
 
     model_config = SettingsConfigDict(env_file=".env")
 
